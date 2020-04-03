13f2d1f40f0001564fa1cb20b7a98d5a6d09f727
sonolio <54865552+sonolio@users.noreply.github.com>
2020-04-02 19:29:11 -0600

    Update scrape_googlesheets_predictions.py

--- scrape_googlesheets_predictions.py
+++ scrape_googlesheets_predictions.py
@@ -5,7 +5,7 @@ import os
 from urllib.request import urlretrieve
 
 #target url for rotogrinder nba player projections
-url = 'https://docs.google.com/spreadsheets/d/1F6tRt7uAJGyNmgitJbBeb-7BGkqzeOH3-VJnHlBTyPo/edit#gid=54487280'
+url = 'https://docs.google.com/spreadsheets/d/1F6tRt7uAJGyNmgitJbBeb-7BGkqzeOH3-VJnHlBTyPo/edit#gid=966124798'
 destination = 'googlesheets_predictions.csv'
 
 #scraping function for target url
